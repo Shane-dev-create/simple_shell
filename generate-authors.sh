a63ae18d858ab17002982eb31513206f9074bef1
Gilbert Segakweng <shn.shane@gmail.com>
2022-11-16 08:30:53 -0800

    generate authors with bash script

--- generate-authors.sh
+++ generate-authors.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+cat <<- 'EOH'
+	# This file lists all individuals having contributed content to the repository.
+	# For how it is generated, see `hack/generate-authors.sh`.
+EOH
+
 git shortlog -se \
   | perl -spe 's/^\s+\d+\s+//' \
   | sed -e '/^CommitSyncScript.*$/d' \
